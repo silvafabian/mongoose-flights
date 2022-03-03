19667a52b91e436fb4c0faf8b7d43ec8447f2186
Fabian Silva <silvafabian74@yahoo.com>
2022-03-02 19:00:15 -0500

    solve part 2 of lab

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,17 +2,18 @@ import { Flight } from '../models/flight.js'
 
 function newFlight(req, res) {
   res.render('flights/new', {
-    title: 'Add Flight'
+    title: 'Add Flight',
   })
 }
 
 function create(req, res) {
+  console.log('this is create');
   for (let key in req.body) {
     if (req.body[key] === '') delete req.body[key]
 	}
   const flight = new Flight(req.body)
   flight.save(function(err) {
-		if (err) return res.render('flights/new')
+		if (err) return res.redirect('/flights/new')
     res.redirect('/flights')
   })
 }
